809eb85b66e94225e60953281bbf4b7f129b52af
donShapoklyak <vholm368@gmail.com>
2024-11-29 21:11:32 +1000

    ыыыы

--- testWB/hello/views.py
+++ testWB/hello/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-   
+ 
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
-  
-def about(request, name, age):
+    host = request.META["HTTP_HOST"] # получаем адрес сервера
+    user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные бразера
+    path = request.path     # получаем запрошенный путь
+     
     return HttpResponse(f"""
-            <h2>О пользователе</h2>
-            <p>Имя: {name}</p>
-            <p>Возраст: {age}</p>
+        <p>Host: {host}</p>
+        <p>Path: {path}</p>
+        <p>User-agent: {user_agent}</p>
     """)
--- testWB/testWB/urls.py
+++ testWB/testWB/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from hello import views
-  
+ 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about, kwargs={"name":"Tom", "age": 38}),
+    path("", views.index),
 ]
